d19c1d0a3d8eb25f8b849654aaaf2c08cef1af61
Mateusz Naklicki <mateusz.naklicki06@gmail.com>
2021-06-15 21:04:04 +0200

    Add a movement to test players to catch a bug

--- Objects/TestScene.gd
+++ Objects/TestScene.gd
@@ -5,8 +5,11 @@ onready var world = $World
 
 onready var player_scene = load("res://Objects/Player/Player.tscn")
 
-const test_players = 5
+const test_players = 30
+var i = 0
+var vector = "UP"
 
+var players = []
 
 func _ready():
 	var server = NetworkedMultiplayerENet.new()
@@ -14,6 +17,7 @@ func _ready():
 	get_tree().network_peer = server
 #	ball.start_position = Vector2(200, 200)
 #	ball.position = Vector2(200, 200)
+	ball.speed = 60000
 	var player_names = []
 	for i in range(1, test_players+1):
 		var player = player_scene.instance()
@@ -21,5 +25,20 @@ func _ready():
 		player.set_network_master(i) 
 		world.add_child(player)
 		player_names.append(str(i))
+		players.append(player)
 	world.set_players_positions(player_names)
-	
+
+
+func _process(delta):
+	if i % 25 == 0:
+		if vector == "UP":
+			vector = "DOWN"
+		else:
+			vector = "UP"
+		
+	for player in players:
+		if vector == "UP":
+			player.paddle.move(player.up)
+		else:
+			player.paddle.move(player.down)
+	i += 1
